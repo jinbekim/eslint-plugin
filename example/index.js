35b26de72fcb09587bb891a1e8fe765e83aa3e62
김진범 <lljrwq123@gmail.com>
2025-02-15 17:54:46 +0900

    chore: add example

---
+++ example/index.js
@@ -0,0 +1,16 @@
+const lists = document.getElementsByClassName('example');
+const lists2 = document.getElementsByTagNameNS('example');
+const lists3 = document.getElementsByName('example');
+const list4 = document.getElementsByTagName('example');
+
+document.getElementsByClassName('example').forEach(console.log); // error
+
+{
+  lists2.forEach(console.log); // error
+  {
+    lists?.forEach(console.log); // error
+  }
+  lists3.forEach(console.log); // not error
+}
+
+lists.forEach(console.log); // error
